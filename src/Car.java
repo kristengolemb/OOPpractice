c45af86dd35d7aab0a97fbb0cd59068d57dd0589
Kristen <kristengolemb@gmail.com>
2016-08-26 11:28:05 -0400

    first commit

---
+++ src/Car.java
@@ -0,0 +1,47 @@
+public class Car {
+
+	public String make;
+	public String model;
+	public int year;
+	public double price;
+
+	public Car(String make, String model, int year, double price) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
